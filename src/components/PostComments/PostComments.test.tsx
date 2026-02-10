789b710835c41bca33bec9ce1848fd1be6b73328
Otavio <otavio.zerotwo@gmail.com>
2026-02-09 22:16:36 -0300

    feat: add test PostComments component

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,27 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('Deve adicionar os comentários: "a foto não carregou" e "manda de novo"', () => {
+        render(<PostComment />);
+        fireEvent.change(screen.getByTestId('post-input'), {
+            target: {
+                value: 'a foto não carregou'
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-post-submit'));
+        
+        fireEvent.change(screen.getByTestId('post-input'), {
+            target: {
+                value: 'manda de novo'
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-post-submit'));
+        expect(screen.getByText('a foto não carregou')).toBeInTheDocument();
     });
 });
